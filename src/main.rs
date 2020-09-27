e6840cc73f672b6e96b677294f7afdabff4010d0
ashtneoi <ashtneoi@gmail.com>
2020-09-27 12:13:03 -0700

    Reduce coupling between main() and LifeCell

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-#[derive(Clone)]
-struct LifeCell {
+use core::char;
+use core::convert::TryInto;
+
+#[derive(Clone, Copy)]
+pub struct LifeCell {
     lut_config: u16,
     a_config: u8,
     a_reg: u8,
@@ -8,36 +11,48 @@ struct LifeCell {
 }
 
 impl LifeCell {
-    pub fn step(&mut self, a_in: u8, se_in: u8, le_in: u8) {
+    pub fn step(&mut self, input: Output) {
         assert_eq!(self.a_config >> 1, 0);
         assert_eq!(self.a_reg >> 4, 0);
         assert_eq!(self.s_reg >> 4, 0);
         assert_eq!(self.q_reg >> 4, 0);
 
-        if le_in == 1 || (self.a_reg & 0b0010) >> 1 == 1 {
+        if input.le == 1 || (self.a_reg & 0b0010) >> 1 == 1 {
             self.q_reg = self.s_reg;
         }
-        if se_in == 1 || self.a_reg & 0b0001 == 1 {
+        if input.se == 1 || self.a_reg & 0b0001 == 1 {
             let lut_q = ((self.lut_config >> self.a_reg) & 1) as u8;
             self.s_reg = (self.s_reg >> 1) | (lut_q << 3);
         }
         self.a_reg = match self.a_config {
-            0 => a_in,
+            0 => input.q,
             _ => self.a_reg.wrapping_add(1) & 0xF,
         };
     }
 
-    pub fn q(&self) -> u8 {
-        self.q_reg
+    pub fn output(&self) -> Output {
+        Output {
+            q: self.q_reg,
+            se: (self.a_reg & 0b0100) >> 2,
+            le: (self.a_reg & 0b1000) >> 3,
+        }
     }
 
-    pub fn se_out(&self) -> u8 {
-        (self.a_reg & 0b0100) >> 2
+    pub fn as_char(&self) -> char {
+        let x = self.s_reg as u32;
+        match self.s_reg {
+            0x0..=0x9 => '0' as u32 + x,
+            0xA..=0xF => 'A' as u32 - 0xA + x,
+            _ => unreachable!(),
+        }.try_into().unwrap()
     }
+}
 
-    pub fn le_out(&self) -> u8 {
-        (self.a_reg & 0b1000) >> 3
-    }
+#[derive(Clone, Copy)]
+pub struct Output {
+    q: u8,
+    se: u8,
+    le: u8,
 }
 
 const CELL_COUNT: usize = 64;
@@ -64,29 +79,17 @@ fn main() {
             //v[0].s_reg = 0x3;
         //}
 
-        let v0_a_in = v[CELL_COUNT - 1].q();
-        let v0_se_in = v[CELL_COUNT - 1].se_out();
-        let v0_le_in = v[CELL_COUNT - 1].le_out();
+        let v0_input = v[15].output();
         for i in (0..CELL_COUNT).rev() {
-            let a_in;
-            let se_in;
-            let le_in;
-            if i > 0 {
-                a_in = v[i - 1].q();
-                se_in = v[i - 1].se_out();
-                le_in = v[i - 1].le_out();
-            } else {
-                //a_in = (n & 0xF) as u8;
-                a_in = v0_a_in;
-                se_in = v0_se_in;
-                le_in = v0_le_in;
-                //le_in = 0;
-            }
-            v[i].step(a_in, se_in, le_in);
+            let input = match i {
+                0 => v0_input,
+                _ => v[i - 1].output(),
+            };
+            v[i].step(input);
         }
 
         for i in 0..CELL_COUNT {
-            print!("{:1X}", v[i].s_reg);
+            print!("{}", v[i].as_char());
         }
         print!("\n");
         n = (n + 1) & 0xFF;
